71ba7e7d62b031b8bcf224891f7f286291b65bd5
Mark McGuire <mark.b.mcg@gmail.com>
2015-03-10 00:16:08 -0500

    Add failing test case
    * need to rename paramaters to variables to eliminate confusion

--- tests/test_render_template.py
+++ tests/test_render_template.py
@@ -11,7 +11,16 @@ cloudlets = {'plain.yaml': ('Type: AWS::IAM::InstanceProfile\n'
                               'Properties:\n'
                               '  Path: /\n'
                               '  Roles:\n'
-                              '  - {{role}}\n')}
+                              '  - {{role}}\n'),
+             'typed.yaml': ('Type: AWS::EC2::SecurityGroup\n'
+                            'Properties:\n'
+                            '  VpcId:\n'
+                            '    Ref: VPC\n'
+                            '  SecurityGroupEgress:\n'
+                            '    - IpProtocol: "-1"\n'
+                            '      FromPort: "0"\n'
+                            '      ToPort: "65535"\n'
+                            '      CidrIp: 0.0.0.0/0')}
 
 
 class RenderTemplateTest(unittest.TestCase):
@@ -76,6 +85,39 @@ class RenderTemplateTest(unittest.TestCase):
                                               'Path': '/',
                                               'Roles': ['MuhRole']
                                           }}}}, render_template(self.jenv, template_def))
+
+    def test_render_template_with_resource_param(self):
+        template_def = ('paramed_typed', {
+            'parameters': {
+                'VPC': {
+                    'source': {
+                        'template': 'vpc',
+                        'type': 'resource'
+                    },
+                    'type': 'string',
+                }
+            },
+            'cloudlets': {
+                'typed': None
+            }})
+        self.maxDiff = None
+        self.assertEqual({'AWSTemplateFormatVersion': '2010-09-09',
+                          'Parameters': {
+                              'VPC': {
+                                  'Type': 'String'
+                              }
+                          },
+                          'Resources': {
+                              'typed': {
+                                  'Type': 'AWS::EC2::SecurityGroup',
+                                  'Properties': {
+                                      'VpcId': {'Ref': 'VPC'},
+                                      'SecurityGroupEgress': [
+                                          {'IpProtocol': '-1', 'FromPort': '0', 'ToPort': '65535', 'CidrIp': '0.0.0.0/0'}
+                                      ]
+                                  }
+                              }
+                          }}, render_template(self.jenv, template_def))
 
 
 if __name__ == '__main__':
