3909c9be862612b02309d623f2eee8746864baf4
Mark McGuire <mark.b.mcg@gmail.com>
2015-03-14 14:28:17 -0500

    aws connection testing

--- cloudplate/aws.py
+++ cloudplate/aws.py
@@ -10,15 +10,15 @@ def assume_role(region, role_arn, role_session_name, role_opts=None):
     return assumed_role.credentials
 
 
-def connect_to_cf(region, role_arn=None, role_sesion_name=None, role_opts=None):
+def connect_to_cf(region, role_arn=None, role_session_name=None, role_opts=None):
     connect_opts = {}
     if role_arn:
-        if not role_sesion_name:
-            role_sesion_name = 'cloudplate'
-        creds = assume_role(region, role_arn, role_sesion_name, role_opts=role_opts)
+        if not role_session_name:
+            role_session_name = 'cloudplate'
+        creds = assume_role(region, role_arn, role_session_name, role_opts=role_opts)
         connect_opts['aws_access_key_id'] = creds.access_key
         connect_opts['aws_secret_access_key'] = creds.secret_key
-        connect_opts['security_token'] =  creds.sesion_token
+        connect_opts['security_token'] =  creds.session_token
     return cf.connect_to_region(region, **connect_opts)
 
 
---
+++ tests/test_aws.py
@@ -0,0 +1,58 @@
+import unittest
+import mock
+from boto.sts.credentials import Credentials, AssumedRole
+from cloudplate import aws
+
+
+class MyTestCase(unittest.TestCase):
+    @mock.patch('cloudplate.aws.sts.connect_to_region')
+    def test_assume_role(self, ctr_mock):
+        region = 'us-east-1'
+        role_arn = 'fake-role-arn'
+        session_name = 'my-session'
+        opts = {'opt1': True}
+        conn = mock.MagicMock()
+        ctr_mock.return_value = conn
+        aws.assume_role(region, role_arn, session_name, opts)
+        ctr_mock.assert_called_once_with(region)
+        conn.assume_role.assert_called_once_with(role_arn, session_name, **opts)
+
+    @mock.patch('cloudplate.aws.cf.connect_to_region')
+    def test_connect_wo_role(self, ctr_mock):
+        region = 'us-east-1'
+        defn = {
+            'region': region
+        }
+        aws.connect(defn)
+        ctr_mock.assert_called_once_with(region)
+
+    @mock.patch('cloudplate.aws.cf.connect_to_region')
+    @mock.patch('cloudplate.aws.sts.connect_to_region')
+    def test_connect_with_role(self, stsctr_mock, cfctr_mock):
+        region = 'us-east-1'
+        defn = {
+            'region': region,
+            'role': {
+                'role_arn': 'fake-role-arn',
+                'role_session_name': 'my-session',
+                'role_opts': {
+                    'duration': 1800
+                }
+            }
+        }
+        sts_conn = mock.MagicMock()
+        stsctr_mock.return_value = sts_conn
+        creds = Credentials()
+        role = AssumedRole(credentials=creds)
+        creds.access_key = 'access_key'
+        creds.secret_key = 'secret_key'
+        creds.session_token = 'session_token'
+        sts_conn.assume_role.return_value = role
+        aws.connect(defn)
+        stsctr_mock.assert_called_once_with(region)
+        cfctr_mock.assert_called_once_with(region, aws_access_key_id='access_key', aws_secret_access_key='secret_key',
+                                           security_token='session_token')
+
+
+if __name__ == '__main__':
+    unittest.main()
