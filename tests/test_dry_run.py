087689882cccc4f594191ed03ff896d0c3e50ae9
Mark McGuire <mark.b.mcg@gmail.com>
2015-03-15 21:54:47 -0500

    add dry run test

--- tests/test_dry_run.py
+++ tests/test_dry_run.py
@@ -1,9 +1,12 @@
 import unittest
+import mock
+from cloudplate.aws import dry_run_connection
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class DryRunTest(unittest.TestCase):
+    def test_connection_is_a_mock(self):
+        rv = dry_run_connection({'region': 'us-east-1'})
+        self.assertTrue(issubclass(type(rv), mock.MagicMock))
 
 
 if __name__ == '__main__':
