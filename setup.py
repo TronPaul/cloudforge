8db4509a83d8e93e8509bb0c32e7293fe613e7bd
Mark McGuire <mark.b.mcg@gmail.com>
2015-03-22 11:28:29 -0500

    setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from distutils.core import setup
+#! /usr/bin/env python
+
+from setuptools import setup
 
 setup(
     name='cloudforge',
     version='0.0.1',
-    packages=['cloudplate'],
-    url='http://github.com/TronPaul/cloudplate',
+    packages=['cloudforge'],
+    url='http://github.com/TronPaul/cloudforge',
     license='Apache-2.0',
     author='Mark McGuire',
     author_email='mark.b.mcg@gmail.com',
     description='',
-    requires=['boto', 'jinja2', 'PyYAML', 'mock']
+    install_requires=['boto', 'jinja2', 'PyYAML', 'mock'],
+    entry_points={
+        'console_scripts': ['cloudforge=cloudforge.cli:cloudforge']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console'
+    ]
 )
